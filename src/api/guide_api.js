a383ead66b83319cdd170e48c52b92620390498f
a <a>
2018-04-19 17:15:12 +0800

    添加图片上传接口

--- src/api/guide_api.js
+++ src/api/guide_api.js
@@ -17,8 +17,23 @@ let getPersonInfo=(params)=>{
     params: params
   });
 }
+
+let uploadPic=function(params){
+
+  // let file = e.target.files[0];
+  // let param = new FormData(); //创建form对象
+  // param.append('file', file, file.name); //通过append向form对象添加数据
+  // param.append('chunk', '0'); //添加form表单中其他数据
+  // console.log(param.get('file')); //FormData私有类对象，访问不到，可以通过get判断值是否传进去
+  let config = {
+    headers:{'Content-Type':'multipart/form-data'}
+  }; 
+  return axios.post("",params,config);
+}
+
 export {
   getDemo,
   postDemo,
-  getPersonInfo
+  getPersonInfo,
+  uploadPic
 }
