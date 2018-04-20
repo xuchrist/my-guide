57a49a83d666df0e47d42e2f806a432e2d0005ec
a <a>
2018-04-20 16:47:52 +0800

    visitors news 新闻列表 和详情

---
+++ src/components/visitors/news_info.vue
@@ -0,0 +1,27 @@
+<template>
+   <yd-layout>
+    <v-header slot="navbar" :title="'详情'" to="/visitors_news_list">
+    </v-header> 
+   </yd-layout>
+</template>
+
+
+<script>
+import vFooter from './footer'
+import vHeader from './header'
+export default {
+  name:"news_info",
+  data () {
+    return {
+      
+    }
+  },
+  components: {
+    vHeader
+  },
+  created () {
+     this.obj=this.$route.query.obj;
+     console.log("news info"+ JSON.stringify(this.obj));
+  }
+}
+</script>
---
+++ src/components/visitors/news_list.vue
@@ -0,0 +1,66 @@
+<template>
+  <yd-layout> 
+    <v-header slot="navbar" title="投诉列表" to="/guide_home">
+    </v-header>
+    <yd-list theme=4>
+      <yd-list-item v-for="(item, key) in list" :key="key"  @click.native="goDetail(item)">
+        <img slot="img" :src="item.img">
+        <span slot="title">{{item.title}}</span>
+        <yd-list-other slot="other">
+          <div  class="left">
+            <span class="demo-list-price">
+              <em>¥</em>{{item.price}}</span>
+            <span class="demo-list-del-price">¥{{item.w_price}}</span>
+          </div>
+          <div class="right">content</div>
+        </yd-list-other>
+      </yd-list-item>
+    </yd-list>
+
+    <v-footer slot="tabbar" index="3"></v-footer>
+  </yd-layout>
+</template>
+
+<style scoped>
+  .left{
+    position: absolute;
+    bottom: 0.35em; 
+  }
+  .right{
+    position: absolute;
+    bottom: 0.35em;
+    right: 0;
+  }
+</style>
+
+<script>
+import vHeader from './header'
+import vFooter from './footer'
+export default {
+  name:"news_list",
+   components: {
+    vFooter,
+    vHeader
+  },
+  data () {
+    return {
+      
+    }
+  },methods: {
+    goDetail:function(item){ 
+      this.$router.push({path:'/guide_news_info',query:{obj:item}});//类似get传参，通过URL传递参数
+    // this.$router.push({path:'/guide_cash_info',params:{obj:item}});//类似post传参 
+    }
+  },
+  created () {
+     this.list= [
+                    {img: "//img1.shikee.com/try/2016/06/23/14381920926024616259.jpg", title: "标题111标题标题标题标题", price: 156.23, w_price: 89.36},
+                    {img: "//img1.shikee.com/try/2016/06/21/10172020923917672923.jpg", title: "标题222标题标题标题标题", price: 256.23, w_price: 89.36},
+                    {img: "//img1.shikee.com/try/2016/06/23/15395220917905380014.jpg", title: "标题333标题标题标题标题", price: 356.23, w_price: 89.36},
+                    {img: "//img1.shikee.com/try/2016/06/25/14244120933639105658.jpg", title: "标题444标题标题标题标题", price: 456.23, w_price: 89.36},
+                    {img: "//img1.shikee.com/try/2016/06/26/12365720933909085511.jpg", title: "标题555标题标题标题标题", price: 556.23, w_price: 89.36},
+                    {img: "//img1.shikee.com/try/2016/06/19/09430120929215230041.jpg", title: "标题666标题标题标题标题", price: 656.23, w_price: 89.36}
+                ]
+  }
+}
+</script>
