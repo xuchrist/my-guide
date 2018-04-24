79ed24d781e0116fcca73a56ec98190e25fb55cf
a <a>
2018-04-24 16:14:40 +0800

    Signed-off-by: a <a>

---
+++ src/components/visitors/login.vue
@@ -0,0 +1,10 @@
+<template>
+    <div>
+        visitor login
+    </div>
+</template>
+<script>
+export default {
+  name:"visitor_login"
+}
+</script>
---
+++ src/components/visitors/register.vue
@@ -0,0 +1,10 @@
+<template>
+    <div>
+        visitor register
+    </div>
+</template>
+<script>
+export default {
+  name:"visitor_register"
+}
+</script>
