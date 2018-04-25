5fc8c863a0c198f077ac81e339232cd9cafd22b6
a <a>
2018-04-25 15:57:30 +0800

    aaa

--- src/components/admin/news/news.vue
+++ src/components/admin/news/news.vue
@@ -1,25 +1,22 @@
 <template>
-<yd-layout>
+  <yd-layout>
     <v-header slot="navbar" title="新闻发布" to="/admin_home">
     </v-header>
-    
- 
-     <yd-cell-group>
-            <yd-cell-item>  
-                   <UE slot="right" :defaultMsg=defaultMsg :config=config ref="ue" style="width:100%" ></UE>
-            </yd-cell-item>
-            <yd-cell-item>  
-                  <yd-button slot="right" @click.native="getUEContent" size="large" type="primary" shape="circle">发布</yd-button>
-            </yd-cell-item>
-        </yd-cell-group>
-</yd-layout>
- 
+    <yd-cell-group>
+      <yd-cell-item>
+        <UE slot="right" :defaultMsg=defaultMsg :config=config ref="ue" style="width:100%"></UE>
+      </yd-cell-item>
+      <yd-cell-item>
+        <yd-button slot="right" @click.native="getUEContent" size="large" type="primary" shape="circle">发布</yd-button>
+      </yd-cell-item>
+    </yd-cell-group>
+  </yd-layout>
 </template>
 
 
 <script>
   import UE from '../../common/ue.vue';
-   import vHeader from '../header'
+  import vHeader from '../header'
   import vFooter from '../footer'
   export default {
     name: "admin_news",
@@ -33,22 +30,23 @@
         config: {
           initialFrameWidth: null,
           initialFrameHeight: 350,
-           toolbars: [[  
-          'fullscreen', 'source', '|', 'undo', 'redo', '|',  
-          'bold', 'italic', 'underline',  
-          'removeformat', '|',  
-          'insertorderedlist', 'insertunorderedlist',  
-          'indent', '|', 'justifyleft', 'justifycenter',  
-          '|', 'imagenone', 'imageleft', 'imageright', 'imagecenter', 'simpleupload',  
-          '|', 'link', 'unlink'  
-      ]]  
+          toolbars: [
+            [
+              'fullscreen', 'source', '|', 'undo', 'redo', '|',
+              'bold', 'italic', 'underline',
+              'removeformat', '|',
+              'insertorderedlist', 'insertunorderedlist',
+              'indent', '|', 'justifyleft', 'justifycenter',
+              '|', 'imagenone', 'imageleft', 'imageright', 'imagecenter', 'simpleupload',
+              '|', 'link', 'unlink'
+            ]
+          ]
         }
       }
     },
     methods: {
       getUEContent() {
         let content = this.$refs.ue.getUEContent();
-       
         console.log(content)
       }
     }
